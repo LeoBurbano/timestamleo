54f4c233a78882abeb05b66bb071c422fa9690fa
LeoBurbano <leofr7nco@hotmail.com>
2017-03-05 00:12:03 -0500

    timestamp

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 var express=require('express');
 var app=express();
+var path=require('path');
+var moment=require('moment');
 
 app.set('port', (process.env.PORT || 5000));
-
-app.get('/',function(req,res){
-	res.send('Hello World');
-});
-
 app.listen(app.get('port'),function(){
-	console.log('Node app is running on port', app.get('port'));	
+	console.log('Timestamp Node app is running on port', app.get('port'));	
 });
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('/:datestring',function(req,res){
+	var myDate;
+	if(/^\d{8,}$/.test(req.params.datestring)){
+		myDate=moment(req.params.datestring,"X");
+	}else{
+		myDate=moment(req.params.datestring,"MMMM D, YYYY");
+	}
+	if(myDate.isValid()){
+		res.json({
+			"unix":myDate.format("X"),
+			"natural":myDate.format("MMMM D, YYYY")
+		})
+	}else{
+		res.json({
+			"unix":null,
+			"natural":null
+		});
+
+	}
+
+});
+
