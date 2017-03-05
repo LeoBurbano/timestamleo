c07afc26ebdb84bbe57f39fdebf5160eb3ac50b9
LeoBurbano <leofr7nco@hotmail.com>
2017-03-04 22:25:30 -0500

    process.env port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express=require('express');
 var app=express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/',function(req,res){
 	res.send('Hello World');
 });
 
-app.listen(8080,function(){
-	console.log('Running in 8080 port!');	
+app.listen(app.get('port'),function(){
+	console.log('Node app is running on port', app.get('port'));	
 });
